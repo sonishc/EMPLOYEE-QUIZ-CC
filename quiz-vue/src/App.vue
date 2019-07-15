3b0c578cf659060702654a6d06d6e0f14502e231
Serhii Onishchuk <sonishchuk@solanolabs.com>
2019-07-15 15:56:20 +0300

    Initial commit vue UI

---
+++ quiz-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <IndexQuiz msg="Welcome to EMPLOYEE-QUIZ-CC App"/>
+  </div>
+</template>
+
+<script>
+import IndexQuiz from './components/IndexQuiz.vue'
+
+export default {
+  name: 'app',
+  components: {
+    IndexQuiz
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
