4d7bfe19d818b948dcd382aca8ac39f5b038ffd4
Serhii Onishchuk <sonishchuk@solanolabs.com>
2019-07-16 17:31:38 +0300

    Quiz model && fmt server.go file

--- api/server.go
+++ api/server.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "../api/controllers"
-  "github.com/gorilla/mux"
-  "log"
-  "net/http"
+    "../api/controllers"
+    "github.com/gorilla/mux"
+    "log"
+    "net/http"
 )
 
 func main() {
-  router := mux.NewRouter()
-  router.HandleFunc("/", controllers.GetQuestions).Methods("GET")
-  router.HandleFunc("/question/{id}", controllers.GetQuestion).Methods("GET")
-  router.HandleFunc("/question", controllers.CreateQuestion).Methods("POST")
-  router.HandleFunc("/question/{id}", controllers.UpdateQuestion).Methods("PUT")
-  router.HandleFunc("/question/{id}", controllers.DeleteQuestion).Methods("DELETE")
+    router := mux.NewRouter()
+    router.HandleFunc("/", controllers.GetQuestions).Methods("GET")
+    router.HandleFunc("/question/{id}", controllers.GetQuestion).Methods("GET")
+    router.HandleFunc("/question", controllers.CreateQuestion).Methods("POST")
+    router.HandleFunc("/question/{id}", controllers.UpdateQuestion).Methods("PUT")
+    router.HandleFunc("/question/{id}", controllers.DeleteQuestion).Methods("DELETE")
 
-  log.Fatal(http.ListenAndServe(":8000", router))
+    log.Fatal(http.ListenAndServe(":8000", router))
 }
