edf5a120957cb264d932b5d36a0bcbcdf85e84d4
Vitalii Kostyrka <vkosturka@solanolabs.com>
2019-07-18 12:59:53 +0300

    Make CRUD for question model

--- api/controllers/questionController.go
+++ api/controllers/questionController.go
@@ -2,12 +2,51 @@ package controllers
 
 import (
 	"../db"
+	"../models"
 	"../utils"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func GetQuestions(w http.ResponseWriter, r *http.Request) {
 	utils.SetHeaders(w)
 	json.NewEncoder(w).Encode(db.GetQuestions())
+}
+
+func GetQuestion(w http.ResponseWriter, r *http.Request) {
+	utils.SetHeaders(w)
+	params := mux.Vars(r)
+	json.NewEncoder(w).Encode(db.GetQuestion(params["id"]))
+}
+
+func CreateQuestion(w http.ResponseWriter, r *http.Request) {
+	utils.SetHeaders(w)
+	var question models.Question
+	_ = json.NewDecoder(r.Body).Decode(&question)
+	message, status := question.Validate()
+	if status {
+		db.CreateQuestion(&question)
+	}
+	json.NewEncoder(w).Encode(message)
+}
+
+func UpdateQuestion(w http.ResponseWriter, r *http.Request) {
+	utils.SetHeaders(w)
+	params := mux.Vars(r)
+	question := db.GetQuestion(params["id"])
+	_ = json.NewDecoder(r.Body).Decode(&question)
+	message, status := question.Validate()
+	if status {
+		db.UpdateQuestion(question)
+	}
+	json.NewEncoder(w).Encode(message)
+}
+
+func DeleteQuestion(w http.ResponseWriter, r *http.Request) {
+	utils.SetHeaders(w)
+	params := mux.Vars(r)
+	question := db.GetQuestion(params["id"])
+	db.DeleteQuestion(question)
+	json.NewEncoder(w).Encode("success")
 }
--- api/server.go
+++ api/server.go
@@ -10,6 +10,10 @@ import (
 func main() {
   router := mux.NewRouter()
   router.HandleFunc("/", controllers.GetQuestions).Methods("GET")
+  router.HandleFunc("/question/{id}", controllers.GetQuestion).Methods("GET")
+  router.HandleFunc("/question", controllers.CreateQuestion).Methods("POST")
+  router.HandleFunc("/question/{id}", controllers.UpdateQuestion).Methods("PUT")
+  router.HandleFunc("/question/{id}", controllers.DeleteQuestion).Methods("DELETE")
 
   log.Fatal(http.ListenAndServe(":8000", router))
 }
